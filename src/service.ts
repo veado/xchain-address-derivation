80e936a9b57a1d3118ac09f40919e287209949fe
veado <61792675+veado@users.noreply.github.com>
2023-03-18 19:46:03 +0100

    Add BNB (#5)
    incl. `BETA` label in `Hero`

--- src/service.ts
+++ src/service.ts
@@ -1,14 +1,20 @@
+import { BNBChain, Client as BnbClient } from "@xchainjs/xchain-binance";
 import { Client as MayaClient, MAYAChain } from "@xchainjs/xchain-mayachain";
 import { Client as ThorClient, THORChain } from "@xchainjs/xchain-thorchain";
 import { delay } from "@xchainjs/xchain-util";
-import { Chain, GetAddressParams, Network } from "./types";
-import { getRootDerivationPath, toClientNetwork } from "./util/common";
+import { GetAddressParams } from "./types";
+import {
+  getRootDerivationPath,
+  toClientNetwork,
+  getDerivationPathIndex,
+} from "./util/common";
 
 const getMayaAddress = async ({
   network,
   phrase,
   path,
-}: Omit<GetAddressParams, "chain">): Promise<string> => {
+  chain,
+}: GetAddressParams): Promise<string> => {
   try {
     const rootDerivationPath = getRootDerivationPath(path);
     const client = new MayaClient({
@@ -22,10 +28,10 @@ const getMayaAddress = async ({
     });
     // delay to relax UI
     await delay(300);
-    const address = client.getAddress(path[4]);
+    const address = client.getAddress(getDerivationPathIndex(path));
     return address;
   } catch (e) {
-    console.error("getAddress Maya error:", e);
+    console.error(`getAddress ${chain} error: ${e}`);
     return e;
   }
 };
@@ -34,7 +40,8 @@ const getTHORChainAddress = async ({
   network,
   phrase,
   path,
-}: Omit<GetAddressParams, "chain">): Promise<string> => {
+  chain,
+}: GetAddressParams): Promise<string> => {
   try {
     const rootDerivationPath = getRootDerivationPath(path);
     const client = new ThorClient({
@@ -48,10 +55,37 @@ const getTHORChainAddress = async ({
     });
     // delay to relax UI
     await delay(300);
-    const address = client.getAddress(path[4]);
+    const address = client.getAddress(getDerivationPathIndex(path));
     return address;
   } catch (e) {
-    console.error("getAddress THOR error:", e);
+    console.error(`getAddress ${chain} error: ${e}`);
+    return e;
+  }
+};
+
+const getBnbAddress = async ({
+  network,
+  phrase,
+  path,
+  chain,
+}: GetAddressParams): Promise<string> => {
+  try {
+    const rootDerivationPath = getRootDerivationPath(path);
+    const client = new BnbClient({
+      network: toClientNetwork(network),
+      phrase,
+      rootDerivationPaths: {
+        mainnet: rootDerivationPath,
+        stagenet: rootDerivationPath,
+        testnet: null,
+      },
+    });
+    // delay to relax UI
+    await delay(300);
+    const address = client.getAddress(getDerivationPathIndex(path));
+    return address;
+  } catch (e) {
+    console.error(`getAddress ${chain} error: ${e}`);
     return e;
   }
 };
@@ -61,11 +95,13 @@ export const getAddress = ({
   phrase,
   path,
   chain,
-}: GetAddressParams & { chain: Chain }): Promise<string> => {
+}: GetAddressParams): Promise<string> => {
   switch (chain) {
     case THORChain:
-      return getTHORChainAddress({ network, phrase, path });
+      return getTHORChainAddress({ network, phrase, path, chain });
     case MAYAChain:
-      return getMayaAddress({ network, phrase, path });
+      return getMayaAddress({ network, phrase, path, chain });
+    case BNBChain:
+      return getBnbAddress({ network, phrase, path, chain });
   }
 };
