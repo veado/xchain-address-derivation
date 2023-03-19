687042b6edb2e7a7ab8a01998b588ed9e74d4ffe
Veado <veado@protonmail.com>
2023-03-19 11:18:57 +0100

    DRY services

--- src/service.ts
+++ src/service.ts
@@ -1,6 +1,14 @@
-import { AVAXChain, Client as AvaxClient, defaultAvaxParams } from "@xchainjs/xchain-avax";
+import {
+  AVAXChain,
+  Client as AvaxClient,
+  defaultAvaxParams,
+} from "@xchainjs/xchain-avax";
 import { BNBChain, Client as BnbClient } from "@xchainjs/xchain-binance";
-import { BSCChain, Client as BscClient, defaultBscParams } from "@xchainjs/xchain-bsc";
+import {
+  BSCChain,
+  Client as BscClient,
+  defaultBscParams,
+} from "@xchainjs/xchain-bsc";
 import { Client as GaiaClient, GAIAChain } from "@xchainjs/xchain-cosmos";
 import { Client as EthClient, ETHChain } from "@xchainjs/xchain-ethereum";
 import { Client as MayaClient, MAYAChain } from "@xchainjs/xchain-mayachain";
@@ -11,114 +19,89 @@ import {
   getRootDerivationPath,
   toClientNetwork,
   getDerivationPathIndex,
+  chainToString,
 } from "./util/common";
 
 const getMayaAddress = async ({
   network,
   phrase,
   path,
-  chain,
 }: GetAddressParams): Promise<string> => {
-  try {
-    const rootDerivationPath = getRootDerivationPath(path);
-    const client = new MayaClient({
-      network: toClientNetwork(network),
-      phrase,
-      rootDerivationPaths: {
-        mainnet: rootDerivationPath,
-        stagenet: rootDerivationPath,
-        testnet: null,
-      },
-    });
-    // delay to relax UI
-    await delay(300);
-    const address = client.getAddress(getDerivationPathIndex(path));
-    return address;
-  } catch (e) {
-    console.error(`getAddress ${chain} error: ${e}`);
-    return e;
-  }
+  const rootDerivationPath = getRootDerivationPath(path);
+  const client = new MayaClient({
+    network: toClientNetwork(network),
+    phrase,
+    rootDerivationPaths: {
+      mainnet: rootDerivationPath,
+      stagenet: rootDerivationPath,
+      testnet: null,
+    },
+  });
+  await delay(300);
+  const index = getDerivationPathIndex(path);
+  return client.getAddress(index);
 };
 
 const getTHORChainAddress = async ({
   network,
   phrase,
   path,
-  chain,
 }: GetAddressParams): Promise<string> => {
-  try {
-    const rootDerivationPath = getRootDerivationPath(path);
-    const client = new ThorClient({
-      network: toClientNetwork(network),
-      phrase,
-      rootDerivationPaths: {
-        mainnet: rootDerivationPath,
-        stagenet: rootDerivationPath,
-        testnet: null,
-      },
-    });
-    // delay to relax UI
-    await delay(300);
-    const address = client.getAddress(getDerivationPathIndex(path));
-    return address;
-  } catch (e) {
-    console.error(`getAddress ${chain} error: ${e}`);
-    return e;
-  }
+  const rootDerivationPath = getRootDerivationPath(path);
+  const client = new ThorClient({
+    network: toClientNetwork(network),
+    phrase,
+    rootDerivationPaths: {
+      mainnet: rootDerivationPath,
+      stagenet: rootDerivationPath,
+      testnet: null,
+    },
+  });
+  await delay(300);
+  const index = getDerivationPathIndex(path);
+  return client.getAddress(index);
 };
 
 const getBnbAddress = async ({
   network,
   phrase,
   path,
-  chain,
 }: GetAddressParams): Promise<string> => {
-  try {
-    const rootDerivationPath = getRootDerivationPath(path);
-    const client = new BnbClient({
-      network: toClientNetwork(network),
-      phrase,
-      rootDerivationPaths: {
-        mainnet: rootDerivationPath,
-        stagenet: rootDerivationPath,
-        testnet: null,
-      },
-    });
-    // delay to relax UI
-    await delay(300);
-    const address = client.getAddress(getDerivationPathIndex(path));
-    return address;
-  } catch (e) {
-    console.error(`getAddress ${chain} error: ${e}`);
-    return e;
-  }
+  const rootDerivationPath = getRootDerivationPath(path);
+  const client = new BnbClient({
+    network: toClientNetwork(network),
+    phrase,
+    rootDerivationPaths: {
+      mainnet: rootDerivationPath,
+      stagenet: rootDerivationPath,
+      testnet: null,
+    },
+  });
+  // delay to relax UI
+  await delay(300);
+  const index = getDerivationPathIndex(path);
+  return client.getAddress(index);
 };
 
 const getGaiaAddress = async ({
   network,
   phrase,
   path,
-  chain,
 }: GetAddressParams): Promise<string> => {
-  try {
-    const rootDerivationPath = getRootDerivationPath(path);
-    const client = new GaiaClient({
-      network: toClientNetwork(network),
-      phrase,
-      rootDerivationPaths: {
-        mainnet: rootDerivationPath,
-        stagenet: rootDerivationPath,
-        testnet: null,
-      },
-    });
-    // delay to relax UI
-    await delay(300);
-    const address = client.getAddress(getDerivationPathIndex(path));
-    return address;
-  } catch (e) {
-    console.error(`getAddress ${chain} error: ${e}`);
-    return e;
-  }
+  const rootDerivationPath = getRootDerivationPath(path);
+  const client = new GaiaClient({
+    network: toClientNetwork(network),
+    phrase,
+    rootDerivationPaths: {
+      mainnet: rootDerivationPath,
+      stagenet: rootDerivationPath,
+      testnet: null,
+    },
+  });
+  // delay to relax UI
+  await delay(300);
+  const index = getDerivationPathIndex(path);
+  return client.getAddress(index);
 };
 
 const getEthAddress = async ({
@@ -127,81 +110,64 @@ const getEthAddress = async ({
   path,
   chain,
 }: GetAddressParams): Promise<string> => {
-  try {
-    const rootDerivationPath = getRootDerivationPath(path);
-    const client = new EthClient({
-      network: toClientNetwork(network),
-      phrase,
-      rootDerivationPaths: {
-        mainnet: rootDerivationPath,
-        stagenet: rootDerivationPath,
-        testnet: null,
-      },
-    });
-    // delay to relax UI
-    await delay(300);
-    const address = client.getAddress(getDerivationPathIndex(path));
-    return address;
-  } catch (e) {
-    console.error(`getAddress ${chain} error: ${e}`);
-    return e;
-  }
+  const rootDerivationPath = getRootDerivationPath(path);
+  const client = new EthClient({
+    network: toClientNetwork(network),
+    phrase,
+    rootDerivationPaths: {
+      mainnet: rootDerivationPath,
+      stagenet: rootDerivationPath,
+      testnet: null,
+    },
+  });
+  // delay to relax UI
+  await delay(300);
+  const index = getDerivationPathIndex(path);
+  return client.getAddress(index);
 };
 
 const getBscAddress = async ({
   network,
   phrase,
   path,
-  chain,
 }: GetAddressParams): Promise<string> => {
-  try {
-    const rootDerivationPath = getRootDerivationPath(path);
-    const client = new BscClient({
-      ...defaultBscParams,
-      network: toClientNetwork(network),
-      phrase,
-      rootDerivationPaths: {
-        mainnet: rootDerivationPath,
-        stagenet: rootDerivationPath,
-        testnet: null,
-      },
-    });
-    // delay to relax UI
-    await delay(300);
-    const address = client.getAddress(getDerivationPathIndex(path));
-    return address;
-  } catch (e) {
-    console.error(`getAddress ${chain} error: ${e}`);
-    return e;
-  }
+  const rootDerivationPath = getRootDerivationPath(path);
+  const client = new BscClient({
+    ...defaultBscParams,
+    network: toClientNetwork(network),
+    phrase,
+    rootDerivationPaths: {
+      mainnet: rootDerivationPath,
+      stagenet: rootDerivationPath,
+      testnet: null,
+    },
+  });
+  // delay to relax UI
+  await delay(300);
+  const index = getDerivationPathIndex(path);
+  return client.getAddress(index);
 };
 
 const getAvaxAddress = async ({
   network,
   phrase,
   path,
-  chain,
 }: GetAddressParams): Promise<string> => {
-  try {
-    const rootDerivationPath = getRootDerivationPath(path);
-    const client = new AvaxClient({
-      ...defaultAvaxParams,
-      network: toClientNetwork(network),
-      phrase,
-      rootDerivationPaths: {
-        mainnet: rootDerivationPath,
-        stagenet: rootDerivationPath,
-        testnet: null,
-      },
-    });
-    // delay to relax UI
-    await delay(300);
-    const address = client.getAddress(getDerivationPathIndex(path));
-    return address;
-  } catch (e) {
-    console.error(`getAddress ${chain} error: ${e}`);
-    return e;
-  }
+  const rootDerivationPath = getRootDerivationPath(path);
+  const client = new AvaxClient({
+    ...defaultAvaxParams,
+    network: toClientNetwork(network),
+    phrase,
+    rootDerivationPaths: {
+      mainnet: rootDerivationPath,
+      stagenet: rootDerivationPath,
+      testnet: null,
+    },
+  });
+  // delay to relax UI
+  await delay(300);
+  const index = getDerivationPathIndex(path);
+  return client.getAddress(index);
 };
 
 export const getAddress = ({
@@ -210,20 +176,27 @@ export const getAddress = ({
   path,
   chain,
 }: GetAddressParams): Promise<string> => {
-  switch (chain) {
-    case THORChain:
-      return getTHORChainAddress({ network, phrase, path, chain });
-    case MAYAChain:
-      return getMayaAddress({ network, phrase, path, chain });
-    case BNBChain:
-      return getBnbAddress({ network, phrase, path, chain });
-    case GAIAChain:
-      return getGaiaAddress({ network, phrase, path, chain });
-    case ETHChain:
-      return getEthAddress({ network, phrase, path, chain });
-    case BSCChain:
-      return getBscAddress({ network, phrase, path, chain });
-    case AVAXChain:
-      return getAvaxAddress({ network, phrase, path, chain });
-  }
+  const _getAddress = () => {
+    switch (chain) {
+      case THORChain:
+        return getTHORChainAddress({ network, phrase, path, chain });
+      case MAYAChain:
+        return getMayaAddress({ network, phrase, path, chain });
+      case BNBChain:
+        return getBnbAddress({ network, phrase, path, chain });
+      case GAIAChain:
+        return getGaiaAddress({ network, phrase, path, chain });
+      case ETHChain:
+        return getEthAddress({ network, phrase, path, chain });
+      case BSCChain:
+        return getBscAddress({ network, phrase, path, chain });
+      case AVAXChain:
+        return getAvaxAddress({ network, phrase, path, chain });
+    }
+  };
+
+  return _getAddress().catch((e) => {
+    console.error(`getAddress for ${chainToString(chain)} failed. ${e}`);
+    return e;
+  });
 };
